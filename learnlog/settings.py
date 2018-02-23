27e971bd0e1e649db2540cbc988a75e7a163d602
ugeg <ugeg@outlook.com>
2018-02-23 13:56:23 +0800

    set debug false

--- learnlog/settings.py
+++ learnlog/settings.py
@@ -133,7 +133,8 @@ if 'HOME' in os.environ:
             'default':dj_database_url.config(default='postgres://localhost')
         }
         SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO','https')
-        ALLOWED_HOSTS = '*'
+        ALLOWED_HOSTS = ['learnlogtest.herokuapp.com']
+		DEBUG = False
         BASE_DIR = os.path.dirname(os.path.abspath(__file__))
         STATIC_ROOT = 'staticfiles'
         STATICFILES_DIRS = (
