043244dad5459e0f48c66db3e5da24fbbbef744a
ugeg <ugeg@outlook.com>
2018-02-23 13:15:10 +0800

    '4'

--- learnlog/settings.py
+++ learnlog/settings.py
@@ -124,18 +124,19 @@ STATIC_URL = '/static/'
 LOGIN_URL = '/users/login/'
 BOOTSTRAP3 = {'include_jquery': True}
 
-# Herokuè®¾ç½®
-if os.environ['HOME'] == '/app':
-#if os.getcwd() == 'app/':
-    import dj_database_url
-    DATABASES = {
-        'default':dj_database_url.config(default='postgres://localhost')
-    }
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO','https')
-    ALLOWED_HOSTS = '*'
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'staticfiles'
-    STATICFILES_DIRS = (
-            os.path.join(BASE_DIR, 'static'),
-)	
+# HerokuÉèÖÃ
+if 'HOME' in os.environ:
+    if os.environ['HOME'] == '/app':
+    #if os.getcwd() == 'app/':
+        import dj_database_url
+        DATABASES = {
+            'default':dj_database_url.config(default='postgres://localhost')
+        }
+        SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO','https')
+        ALLOWED_HOSTS = '*'
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        STATIC_ROOT = 'staticfiles'
+        STATICFILES_DIRS = (
+                os.path.join(BASE_DIR, 'static'),
+    )	
 
