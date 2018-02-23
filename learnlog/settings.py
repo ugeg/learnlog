6302ac70473f13dbf00daee5f9a171edc1dd75c8
ugeg <ugeg@outlook.com>
2018-02-23 12:02:43 +0800

    '4'

--- learnlog/settings.py
+++ learnlog/settings.py
@@ -134,8 +134,8 @@ if os.getcwd() == 'app/':
     ALLOWED_HOSTS = '*'
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = 'staticfiles'
-	STATICFILES_DIRS = (
-		os.path.join(BASE_DIR, 'static'),
+    STATICFILES_DIRS = (
+            os.path.join(BASE_DIR, 'static'),
 )	
 '''
 # Static files (CSS, JavaScript, Images)
